a7c398584dc3496da1e9c617b416b634c198a91a
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-17 20:03:55 +0100

    choixMenu(): faire un choix dans le menu

--- ProjetSansPOO/src/fr/projet/commerce/Menu.java
+++ ProjetSansPOO/src/fr/projet/commerce/Menu.java
@@ -17,4 +17,37 @@ public class Menu {
 		System.out.println("6 - afficher le facturier");
 		System.out.println("7 - sortir du programme");
 	}
+	
+	public void choixMenu(int choix) {
+		switch (choix) {
+		case 1:
+			System.out.println("affiche produit");
+			break;
+		case 2:
+			System.out.println("ajouter produit");
+			break;
+		case 3:
+			System.out.println("afficher catalogue");
+			break;
+		case 4:
+			System.out.println("afficher une facture");
+			break;
+		case 5:
+			System.out.println("ajouter une facture");
+			break;
+		case 6:
+			System.out.println("afficher le facturier");
+			break;
+		case 7:
+			sortir();
+		default:
+			break;
+		}
+	}
+	
+	public void sortir() {
+		System.out.println("Au revoir !!!");
+		System.exit(0);
+	}
+	
 }
