e265de42a0d13817e21e9fe8dae430328b3d8455
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-17 22:06:52 +0100

    màj choix menu & affichage produit

--- ProjetSansPOO/src/fr/projet/commerce/Menu.java
+++ ProjetSansPOO/src/fr/projet/commerce/Menu.java
@@ -7,37 +7,38 @@ public class Menu {
 	public static void main(String[] args) {
 		Scanner clavier = new Scanner(System.in);
 		int choix;
-		afficheMenu();
-		System.out.print("Faites votre choix : ");
-		System.out.println();
-		choix = clavier.nextInt();
-		switch (choix) {
-		case 1:
-			System.out.print("Entrer le nom du produit: ");
-			String nom = clavier.nextLine();
-			afficheProduit(nom);
-			break;
-		case 2:
-			System.out.println("ajouter produit");
-			break;
-		case 3:
-			System.out.println("afficher catalogue");
-			break;
-		case 4:
-			System.out.println("afficher une facture");
-			break;
-		case 5:
-			System.out.println("ajouter une facture");
-			break;
-		case 6:
-			System.out.println("afficher le facturier");
-			break;
-		case 7:
-			sortir();
-		default:
-			break;
-		}
-		
+		do {
+			afficheMenu();
+			System.out.print("Faites votre choix : ");
+			choix = clavier.nextInt();
+			clavier.nextLine();
+			switch (choix) {
+			case 1:
+				System.out.print("Entrer le nom du produit: ");
+				String nom = clavier.nextLine();
+				Menu.afficheProduit(nom);
+				break;
+			case 2:
+				System.out.println("ajouter produit");
+				break;
+			case 3:
+				System.out.println("afficher catalogue");
+				break;
+			case 4:
+				System.out.println("afficher une facture");
+				break;
+			case 5:
+				System.out.println("ajouter une facture");
+				break;
+			case 6:
+				System.out.println("afficher le facturier");
+				break;
+			case 7:
+				sortir();
+			default:
+				break;
+			}
+		} while (choix != 7);		
 	}
 	
 	public static void afficheMenu() {
@@ -53,8 +54,12 @@ public class Menu {
 	
 	private static void afficheProduit(String nom) {
 		int numProduit = Catalogue.chercher(nom);
-		System.out.println(
-				numProduit + " : " + nom + " --> " + Catalogue.getPrix(numProduit));
+		if (numProduit == -1) {
+			System.out.println("Référence non cataloguée");
+		} else {
+			System.out.println("article n°" + numProduit + 
+					" : " + nom + " --> " + Catalogue.getPrix(numProduit));
+		}
 	}
 	
 	private static void sortir() {
