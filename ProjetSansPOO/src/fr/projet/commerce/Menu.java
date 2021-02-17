d4c27adb1664a7684d24e990f919e7c66172a8a6
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-17 21:24:31 +0100

    mise à jour choix menu

--- ProjetSansPOO/src/fr/projet/commerce/Menu.java
+++ ProjetSansPOO/src/fr/projet/commerce/Menu.java
@@ -1,27 +1,21 @@
 package fr.projet.commerce;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
+		Scanner clavier = new Scanner(System.in);
+		int choix;
 		afficheMenu();
-		
-	}
-	
-	public static void afficheMenu() {
-		System.out.println("---------------- MENU ----------------");
-		System.out.println("1 - afficher un produit");
-		System.out.println("2 - ajouter un produit");
-		System.out.println("3 - afficher le catalogue");
-		System.out.println("4 - afficher une facture");
-		System.out.println("5 - ajouter une facture");
-		System.out.println("6 - afficher le facturier");
-		System.out.println("7 - sortir du programme");
-	}
-	
-	public void choixMenu(int choix) {
+		System.out.print("Faites votre choix : ");
+		System.out.println();
+		choix = clavier.nextInt();
 		switch (choix) {
 		case 1:
-			//afficheProduit(nom);
+			System.out.print("Entrer le nom du produit: ");
+			String nom = clavier.nextLine();
+			afficheProduit(nom);
 			break;
 		case 2:
 			System.out.println("ajouter produit");
@@ -43,15 +37,27 @@ public class Menu {
 		default:
 			break;
 		}
+		
 	}
 	
-	private void afficheProduit(String nom) {
+	public static void afficheMenu() {
+		System.out.println("---------------- MENU ----------------");
+		System.out.println("1 - afficher un produit");
+		System.out.println("2 - ajouter un produit");
+		System.out.println("3 - afficher le catalogue");
+		System.out.println("4 - afficher une facture");
+		System.out.println("5 - ajouter une facture");
+		System.out.println("6 - afficher le facturier");
+		System.out.println("7 - sortir du programme");
+	}
+	
+	private static void afficheProduit(String nom) {
 		int numProduit = Catalogue.chercher(nom);
 		System.out.println(
 				numProduit + " : " + nom + " --> " + Catalogue.getPrix(numProduit));
 	}
 	
-	private void sortir() {
+	private static void sortir() {
 		System.out.println("Au revoir !!!");
 		System.exit(0);
 	}
