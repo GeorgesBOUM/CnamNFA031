3435824417449b3aac655ae1decbae1b722eb546
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-23 08:53:40 +0100

    Catalogue: chercherProduit()

--- ProjetAvecPOO/src/fr/projet/commerce/Catalogue.java
+++ ProjetAvecPOO/src/fr/projet/commerce/Catalogue.java
@@ -1,6 +1,8 @@
 package fr.projet.commerce;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Optional;
 
 public final class Catalogue {
 	private ArrayList<Produit> listeDeProduits;
@@ -19,11 +21,27 @@ public final class Catalogue {
 	}
 	
 	public void ajouterProduit(String nom, double prix) {
-		Produit produit = chercherProduit(nom);
-		if (produit == null) {
+		Optional<Produit> produit = chercherProduit(nom);
+		if (produit.isPresent()) {
 			listeDeProduits.add(new Produit(nom, prix));
 		} else {
 			System.out.println("Ce produit est déjà catalogué");
 		}
+	}
+	
+	public Optional<Produit> chercherProduit(String nom) {
+		Iterator<Produit> iterator = listeDeProduits.iterator();
+		Optional<Produit> produitOptional = Optional.empty();
+		boolean trouve = false;
+		while (iterator.hasNext() && !trouve) {
+			Produit produit = (Produit) iterator.next();
+			if (produit.getNom().equals(nom)) {
+				trouve = true;
+				produitOptional = Optional.of(produit);
+			} else {
+				System.out.println("Catalogue vide");
+			}
+		}
+		return produitOptional;
 	}
 }
