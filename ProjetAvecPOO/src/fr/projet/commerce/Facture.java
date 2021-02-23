6c03bcc4aecbf94cb963f9c7c06b1676f717da80
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-23 16:21:32 +0100

    Facture: isEmpty()

--- ProjetAvecPOO/src/fr/projet/commerce/Facture.java
+++ ProjetAvecPOO/src/fr/projet/commerce/Facture.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 
 public final class Facture {
 	private ArrayList<LigneFacture> listeDesLignes;
+	
+	public void ajouterLigne(Produit p, int quantite) {
+		listeDesLignes.add(new LigneFacture(p, quantite));
+	}
 }
