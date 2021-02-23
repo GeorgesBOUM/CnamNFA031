fde794dc1b722e6e948a3a74ffcb916499830d52
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-23 07:12:46 +0100

    Catalogue: afficherCatalogue()

--- ProjetAvecPOO/src/fr/projet/commerce/Catalogue.java
+++ ProjetAvecPOO/src/fr/projet/commerce/Catalogue.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 public final class Catalogue {
 	private ArrayList<Produit> listeDeProduits;
 	
+	public void afficherCatalogue() {
+		if (listeDeProduits == null) {
+			System.out.println("Catalogue vide");
+		} else {
+			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+			System.out.println("------------- Catalogue -------------");
+			for (int i = 0; i < listeDeProduits.size(); i++) {
+				listeDeProduits.get(i).toString();
+			}
+			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		}
+	}
 }
