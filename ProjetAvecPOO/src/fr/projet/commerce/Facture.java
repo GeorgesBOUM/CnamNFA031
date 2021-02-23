ce4024dc5dd1ebda3c7b08a5c59c385c28cd736e
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-23 16:40:30 +0100

    Facture: toString()

--- ProjetAvecPOO/src/fr/projet/commerce/Facture.java
+++ ProjetAvecPOO/src/fr/projet/commerce/Facture.java
@@ -1,11 +1,27 @@
 package fr.projet.commerce;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public final class Facture {
 	private ArrayList<LigneFacture> listeDesLignes;
 	
 	public void ajouterLigne(Produit p, int quantite) {
 		listeDesLignes.add(new LigneFacture(p, quantite));
+	}
+	
+	public boolean isEmpty() {
+		return listeDesLignes.isEmpty();
+	}
+	
+	public String toString() {
+		Iterator<LigneFacture> iterator = listeDesLignes.iterator();
+		String affichage = "---------------------------------------\n";
+		while (iterator.hasNext()) {
+			LigneFacture ligneFacture = (LigneFacture) iterator.next();
+			affichage += ligneFacture.toString() + "\n";
+		}
+		affichage += "---------------------------------------\n";
+		return affichage;
 	}
 }
