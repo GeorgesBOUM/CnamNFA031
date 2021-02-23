71cbb9784d55ede166e33be54b01bbe2bcd989a6
GeorgesBOUM <boumgeorges@protonmail.com>
2021-02-23 16:52:36 +0100

    Facturier: ajouterFacture()

--- ProjetAvecPOO/src/fr/projet/commerce/Facturier.java
+++ ProjetAvecPOO/src/fr/projet/commerce/Facturier.java
@@ -5,5 +5,7 @@ import java.util.ArrayList;
 public final class Facturier {
 	private ArrayList<Facture> listeDesFactures;
 	
-	
+	public void ajouterFacture(Facture facture) {
+		listeDesFactures.add(facture);
+	}
 }
