29696d562fc2474d2315713d8792098fcd0e0b7c
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-26 17:21:08 +0100

    création de la classe Exception

---
+++ Cours/src/fr/cours/exceptions/LesExceptions.java
@@ -0,0 +1,10 @@
+package fr.cours.exceptions;
+
+public class LesExceptions {
+	
+	public static void main(String[] args) {
+        System.out.println( "BEGIN main" );
+        
+        System.out.println( "END main" );
+    }
+}
