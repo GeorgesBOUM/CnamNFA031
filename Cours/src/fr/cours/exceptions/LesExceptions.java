0864b04c112d8b5f3b339da800262825a865965e
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-27 17:01:24 +0100

    propagation d'une unchecked exception

--- Cours/src/fr/cours/exceptions/LesExceptions.java
+++ Cours/src/fr/cours/exceptions/LesExceptions.java
@@ -2,11 +2,13 @@ package fr.cours.exceptions;
 
 public class LesExceptions {
 	
-    public static void method3() throws Exception {
+//    public static void method3() throws Exception {
+	public static void method3() throws RuntimeException {
         System.out.println( "BEGIN method3" );
         int divisor = (int) (Math.random() * 3);
         if (divisor == 0) {
-			throw new Exception("divisor cannot be nul");
+//			throw new Exception("divisor cannot be nul");
+        	throw new RuntimeException("divisor cannot be nul");
 		} else {
 			int value = 1 / divisor;
 	        System.out.println( "Value == " + value );
@@ -14,13 +16,15 @@ public class LesExceptions {
         System.out.println( "END method3" );
     }
 
-    public static void method2() throws Exception {
+//    public static void method2() throws Exception {
+	public static void method2() {
         System.out.println( "BEGIN method2" );
         method3();
         System.out.println( "END method2" );
     }
     
-    public static void method1() throws Exception {
+//    public static void method1() throws Exception {
+	public static void method1() {
         System.out.println( "BEGIN method1" );
         method2();
         System.out.println( "END method1" );
@@ -28,11 +32,12 @@ public class LesExceptions {
 	
 	public static void main(String[] args) {
         System.out.println( "BEGIN main" );
-        try {
-        	method1();
-		} catch (Exception e) {
-			System.out.println("Division by zero");
-		}
+        method1();
+//        try {
+//        	method1();
+//		} catch (Exception e) {
+//			System.out.println("Division by zero");
+//		}
         System.out.println( "END main" );
     }
 }
