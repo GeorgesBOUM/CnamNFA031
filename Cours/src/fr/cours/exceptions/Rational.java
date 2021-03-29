97044cdad4c6ab7bdf79158278c460facf3cba00
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-29 13:50:26 +0200

    ajout de constructeurs à Rational

--- Cours/src/fr/cours/exceptions/Rational.java
+++ Cours/src/fr/cours/exceptions/Rational.java
@@ -5,15 +5,31 @@ public class Rational {
     private int numerator;
     private int denominator;
     
+    public Rational() {
+        this( 0, 1 );           // Une délégation de constructeur
+    }
+    
+    public Rational( int numerator ) {
+        this( numerator, 1 );   // Une autre délégation de constructeur
+    }
+    
+    public Rational( int numerator, int denominator ) {
+        this.setNumerator( numerator );
+        this.setDenominator( denominator );
+    }
+    
 	public int getNumerator() {
 		return numerator;
 	}
+	
 	public void setNumerator(int numerator) {
 		this.numerator = numerator;
 	}
+	
 	public int getDenominator() {
 		return denominator;
 	}
+	
 	public void setDenominator(int denominator) {
 		if (denominator == 0) {
 			throw new RationalException();
