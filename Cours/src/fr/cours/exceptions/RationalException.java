7d467f5b6f7bff7b28c06cf1952ae48adf55e781
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-29 13:39:19 +0200

    Quelques constructeurs de RationalException

--- Cours/src/fr/cours/exceptions/RationalException.java
+++ Cours/src/fr/cours/exceptions/RationalException.java
@@ -3,5 +3,17 @@ package fr.cours.exceptions;
 public class RationalException extends IllegalArgumentException {
 	
 	private static final long serialVersionUID = 1L;
+	
 
+    public RationalException() {
+        this( "Denominator cannot be zero" );
+    }
+
+    public RationalException( String message ) {
+        super( message );
+    }
+    
+    public RationalException( String message, Throwable cause ) {
+        super( message, cause );
+    }
 }
