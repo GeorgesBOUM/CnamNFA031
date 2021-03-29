f6fcefc1955f3d2314e9f05be91f844fb255a1a0
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-29 13:31:15 +0200

    ajouts de getters et setters à Rational

--- Cours/src/fr/cours/exceptions/Rational.java
+++ Cours/src/fr/cours/exceptions/Rational.java
@@ -5,5 +5,22 @@ public class Rational {
     private int numerator;
     private int denominator;
     
+	public int getNumerator() {
+		return numerator;
+	}
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+	public int getDenominator() {
+		return denominator;
+	}
+	public void setDenominator(int denominator) {
+		if (denominator == 0) {
+			throw new RationalException();
+		} else {
+			this.denominator = denominator;
+		}
+	}
+    
     
 }
