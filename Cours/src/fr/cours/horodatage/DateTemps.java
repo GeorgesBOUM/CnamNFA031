f1c9c085ea8580deb6a7e9c6c0f9f97fa921359c
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-08 10:40:12 +0200

    formatage simple d'une date

--- Cours/src/fr/cours/horodatage/DateTemps.java
+++ Cours/src/fr/cours/horodatage/DateTemps.java
@@ -2,15 +2,21 @@ package fr.cours.horodatage;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTemps {
 	
 	public static void main (String [] args) {
+		
 		LocalDateTime maintenant = LocalDateTime.now();
 		System.out.println("Date et heure courante: " + maintenant);
 		
 		LocalDate aujourdhui = LocalDate.now();
 		System.out.println("Date courante: " + aujourdhui);
+
+		String formatDate = "dd/MM/yyyy HH:mm:ss";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatDate);
+		String dateApresFormatage = maintenant.format(formatter);
 		
 		int annee = aujourdhui.getYear();
 		int mois = aujourdhui.getMonthValue();
@@ -23,5 +29,7 @@ public class DateTemps {
 				);
 		String bissextile = (aujourdhui.isLeapYear()) ? " est une année bissextile" : " n'est pas une année bissextile";
 		System.out.println(annee + bissextile);
+		
+		System.out.println("Nous sommes le " + dateApresFormatage);
 	}
 }
