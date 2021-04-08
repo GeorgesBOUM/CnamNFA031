38f8c2b44f3bc87a90377c6b20fdc1b3acc3094a
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-08 07:26:50 +0200

    récupérer l'heure et la date courante

--- Cours/src/fr/cours/horodatage/DateTemps.java
+++ Cours/src/fr/cours/horodatage/DateTemps.java
@@ -1,5 +1,11 @@
 package fr.cours.horodatage;
 
+import java.time.LocalDateTime;
+
 public class DateTemps {
 	
+	public static void main (String [] args) {
+		LocalDateTime maintenant = LocalDateTime.now();
+		System.out.println("Date et heure courante: " + maintenant);
+	}
 }
