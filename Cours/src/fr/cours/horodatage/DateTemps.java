85091293674787dccd0a16d78c3a6ccb0b76c5d7
GeorgesBOUM <boumgeorges@protonmail.com>
2021-04-08 07:29:19 +0200

    récupérer uniquement la date courante

--- Cours/src/fr/cours/horodatage/DateTemps.java
+++ Cours/src/fr/cours/horodatage/DateTemps.java
@@ -1,5 +1,6 @@
 package fr.cours.horodatage;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class DateTemps {
@@ -7,5 +8,8 @@ public class DateTemps {
 	public static void main (String [] args) {
 		LocalDateTime maintenant = LocalDateTime.now();
 		System.out.println("Date et heure courante: " + maintenant);
+		
+		LocalDate aujourdhui = LocalDate.now();
+		System.out.println("Date courante: " + aujourdhui);
 	}
 }
